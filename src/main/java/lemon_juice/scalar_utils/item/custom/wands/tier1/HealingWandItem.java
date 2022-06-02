75cd92291d531f8a45a35eba60ee7009234d9371
Lemon-Juiced <tbrotheawesome@gmail.com>
2022-06-02 18:27:06 -0400

    Healing Wand Finally Heals User, Only When Used On Block

--- src/main/java/lemon_juice/scalar_utils/item/custom/wands/tier1/HealingWandItem.java
+++ src/main/java/lemon_juice/scalar_utils/item/custom/wands/tier1/HealingWandItem.java
@@ -1,15 +1,9 @@
 package lemon_juice.scalar_utils.item.custom.wands.tier1;
 
-import net.minecraft.stats.Stats;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.item.context.UseOnContext;
 
 public class HealingWandItem extends Item {
 
@@ -18,26 +12,14 @@ public class HealingWandItem extends Item {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
-        ItemStack stack = player.getItemInHand(hand);
+    public InteractionResult useOn(UseOnContext pContext) {
+       if(!pContext.getLevel().isClientSide){
+           Player player = pContext.getPlayer();
+           if(player.getHealth() < player.getMaxHealth()) {
+               player.setHealth(player.getMaxHealth());
+           }
+       }
 
-        if (player.getHealth() < player.getMaxHealth()) {
-            player.startUsingItem(hand);
-            return InteractionResultHolder.success(stack);
-        }
-        return InteractionResultHolder.fail(stack);
-    }
-
-    @Override
-    public ItemStack finishUsingItem(ItemStack stack, Level world, LivingEntity entityLiving) {
-        if(!world.isClientSide) {
-            entityLiving.addEffect(new MobEffectInstance(MobEffects.HEAL));
-
-            if(entityLiving instanceof Player){
-                Player player = (Player) entityLiving;
-                player.awardStat(Stats.ITEM_USED.get(this));
-            }
-        }
-        return stack;
+       return super.useOn(pContext);
     }
 }
